da64b7d470535488401725a9fa5b736044a445d7
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2026-08-06 12:56:07 -0700

    Align Heartwood File Headers

--- tests/test_catalog.py
+++ tests/test_catalog.py
@@ -1,4 +1,4 @@
-# This source file is part of the Heartwood Skills open-source project
+# This source file is part of the Heartwood open-source project
 #
 # SPDX-FileCopyrightText: 2026 Stanford University and the project authors (see CONTRIBUTORS.md)
 #
